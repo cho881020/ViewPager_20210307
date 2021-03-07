b1e5e286da008013b2c8a0930a23a2cdc7258976
조경진 <cho881020@gmail.com>
2021-03-07 13:35:43 +0900

    액티비티에서 어댑터 생성 => 뷰페이저 연결

--- app/src/main/java/com/tjoeun/viewpager_20210307/MainActivity.kt
+++ app/src/main/java/com/tjoeun/viewpager_20210307/MainActivity.kt
@@ -2,8 +2,12 @@ package com.tjoeun.viewpager_20210307
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tjoeun.viewpager_20210307.adapters.MainViewPagerAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
+
+    lateinit var mvpa : MainViewPagerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +21,10 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+        mvpa = MainViewPagerAdapter(supportFragmentManager)
+
+        mainViewPager.adapter = mvpa
 
     }
 
